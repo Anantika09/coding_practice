9c1becc93694b0ad35b05aead6c53d26cd019765
Anantika09 <anantikaag06@gmail.com>
2025-09-27 11:34:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (95.46%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
+    public int findPeakElement(int[] nums) {
         int start=0;
-        int end=arr.length-1;
+        int end=nums.length-1;
         while(start<end){
             int mid=start+(end-start)/2;
-            if(arr[mid]>arr[mid+1]){
+            if(nums[mid]>nums[mid+1]){
                 end=mid;
             }
             else{
