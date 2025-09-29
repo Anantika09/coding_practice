6c089302063dc4aca1e8a0ff806adcba64734ab1
Anantika09 <anantikaag06@gmail.com>
2025-09-29 14:40:53 +0530

    Time: 257 ms (13.84%) | Memory: 45.1 MB (16.30%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(index)){
-                   
                     if(check(board,word,i,j,index)){
                         return true;
                     }
