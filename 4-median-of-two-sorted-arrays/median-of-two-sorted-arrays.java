87dd1f1fffe1eb334d042696e52915ee637ce151
Anantika09 <anantikaag06@gmail.com>
2025-09-30 16:10:29 +0530

    Time: 5 ms (22.78%) | Memory: 45.9 MB (85.38%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -5,7 +5,7 @@ class Solution {
         int[] res=sortedMerge(nums1,nums2);
         double ans=0;
         if(res.length==2){
-
+            
         }
         if(res.length%2==0){
             return (double)(res[(res.length/2)-1]+res[res.length/2])/2;
@@ -13,28 +13,13 @@ class Solution {
         return (double)res[res.length/2];
     }
 
-       int[] sortedMerge(int[] arr1, int[] arr2) {
-        int n=arr1.length;
-        int m=arr2.length;
-        int res[]=new int [m+n];
-        int i=0,j=0,k=0;
-        while(i<n && j<m){
-            if(arr1[i]<arr2[j]){
-                res[k]=arr1[i];
-                i++;
-            }
-            else{
-                res[k]=arr2[j];
-                j++;
-            }
-            k++;
-        }
-        while(i<n){
-            res[k++]=arr1[i++];
-        }
-        while(j<m){
-            res[k++]=arr2[j++];
-        }
+        int[] sortedMerge(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int[] res = new int[m + n];
+        System.arraycopy(arr1, 0, res, 0, n);
+        System.arraycopy(arr2, 0, res, n, m);
+        Arrays.sort(res);
         return res;
     }
 }
