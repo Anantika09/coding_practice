aa734c62fc42e5ba7714dd8eb77ab4998f62c36d
Anantika09 <anantikaag06@gmail.com>
2025-10-01 15:59:51 +0530

    Time: 14 ms (46.13%) | Memory: 44.9 MB (70.23%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -8,17 +8,16 @@ class Solution {
         int[] freq2=new int[26];
         for(int i=0;i<p.length();i++){
             freq1[p.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
             freq2[s.charAt(i)-'a']++;
-        }
-        if(Arrays.equals(freq1,freq2)){
-            ans.add(0);
-        }
-        for(int k=p.length();k<s.length();k++){
-            freq2[s.charAt(k)-'a']++;
-            freq2[s.charAt(k-p.length())-'a']--;
-            if(Arrays.equals(freq1,freq2)){
-                ans.add(k-p.length()+1);
+            if(i<p.length()-1){
+                continue;
             }
+            if(Arrays.equals(freq1,freq2)){
+                ans.add(i-p.length()+1);
+            }
+            freq2[s.charAt(i-p.length()+1)-'a']--;
         }
         return ans;
     }
