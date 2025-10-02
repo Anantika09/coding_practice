e3cd547048c68042233b6fdbe2195f32f7eee3b9
Anantika09 <anantikaag06@gmail.com>
2025-10-03 00:30:35 +0530

    Time: 6 ms (10.49%) | Memory: 46.4 MB (12.90%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,8 +1,13 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m=nums1.length;
-        int n=nums2.length;
-        List<Integer> res=sortedMerge(nums1,nums2);
+        List<Integer> res=new ArrayList<>();
+        for(int n:nums1){
+            res.add(n);
+        }
+        for(int n:nums2){
+            res.add(n);
+        }
+        Collections.sort(res);
         int size=res.size();
         if(size%2==0){
             return(res.get(size/2-1)+res.get(size/2))/2.0;
@@ -10,17 +15,5 @@ class Solution {
         else{
             return res.get(size/2);
         }
-    }
-
-    public static List<Integer> sortedMerge(int[] arr1, int[] arr2) {
-        List<Integer> res=new ArrayList<>();
-        for(int n:arr1){
-            res.add(n);
-        }
-        for(int n:arr2){
-            res.add(n);
-        }
-        Collections.sort(res);
-        return res;
     }
 }
