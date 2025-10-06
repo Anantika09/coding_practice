0dd501f6a35d9802a3b8c81d0bb8c49805cfeb65
Anantika09 <anantikaag06@gmail.com>
2025-10-06 21:59:28 +0530

    Time: 1 ms (92.92%) | Memory: 44.7 MB (46.15%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,16 @@
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> arr=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            arr.add(nums[i]);
-        }
-        fun(arr,ans,new ArrayList<>());
-        return ans;
+class Solution{
+    List<List<Integer>> r=new ArrayList<>();
+    public List<List<Integer>> permute(int[] n){
+        f(n,new boolean[n.length],new ArrayList<>());
+        return r;
     }
-    public void fun(List<Integer> arr,List<List<Integer>> ans,List<Integer> p){
-        if(arr.size()==0){
-            ans.add(new ArrayList<>(p));
-            return;
-        }
-        for(int i=0;i<arr.size();i++){
-            int num=arr.get(i);
-            p.add(num);
-            arr.remove(i);
-            fun(arr,ans,p);
-            arr.add(i,num);
-            p.remove(p.size()-1);
+    void f(int[] n,boolean[] u,List<Integer> p){
+        if(p.size()==n.length){r.add(new ArrayList<>(p));return;}
+        for(int i=0;i<n.length;i++){
+            if(u[i])continue;
+            u[i]=true;p.add(n[i]);
+            f(n,u,p);
+            u[i]=false;p.remove(p.size()-1);
         }
     }
 }
