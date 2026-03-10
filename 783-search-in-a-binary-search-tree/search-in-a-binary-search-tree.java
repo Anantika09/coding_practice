e2591cdcbf562c9b02fd4b8e22bf01ec812a9061
Anantika09 <anantikaag06@gmail.com>
2026-03-10 11:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (99.73%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -14,22 +14,18 @@
  * }
  */
 class Solution {
-    int value = 0;
     public TreeNode searchBST(TreeNode root, int val) {
-        value = val;
-        return getNode(root);
-    }
-    TreeNode getNode(TreeNode root) {
-        if(root != null) {
-            if(root.val == value) {
-                return root;
-            }else if(root.val > value) {
-                return getNode(root.left);
-            } else if(root.val < value) {
-                return getNode(root.right);
-            } else {
-                return null;
-            }
-        } else return null;
+        if(root==null){
+            return null;
+        }
+        if(root.val == val){
+            return root;
+        } 
+        else if (root.val > val) {
+            return searchBST(root.left, val);
+        } 
+        else {
+            return searchBST(root.right, val);
+        }        
     }
 }
