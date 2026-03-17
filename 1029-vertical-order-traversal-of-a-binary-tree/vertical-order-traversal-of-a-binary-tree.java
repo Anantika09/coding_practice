d8e1d574417f3eba65bde328508f1bb6de75a9cf
Anantika09 <anantikaag06@gmail.com>
2026-03-17 12:32:40 +0530

    Time: 4 ms (61.73%) | Memory: 44.6 MB (13.01%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,25 +1,40 @@
 class Solution {
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer,TreeMap<Integer,List<Integer>>> m=new TreeMap<>();
-        fun(root,m,0,0);
-        List<List<Integer>> ans=new ArrayList<>();
-        for(int p:m.keySet()){
-            List<Integer> l=new ArrayList<>();
-            TreeMap<Integer,List<Integer>> level=m.get(p);
-            for(List<Integer> nodes:level.values()){
-                Collections.sort(nodes);
-                l.addAll(nodes);
-            }
-            ans.add(l);
+    public List<List<Integer>> verticalTraversal(TreeNode root){
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
         }
-        return ans;
-    }
-    void fun(TreeNode root,TreeMap<Integer,TreeMap<Integer,List<Integer>>> m,int r,int c){
-        if(root==null) return;
-        TreeMap<Integer,List<Integer>> p=m.computeIfAbsent(c,k->new TreeMap<>());
-        List<Integer> l=p.computeIfAbsent(r,k->new ArrayList<>());
-        l.add(root.val);
-        fun(root.left,m,r+1,c-1);
-        fun(root.right,m,r+1,c+1);
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        Queue<int[]> pos=new LinkedList<>();
+        q.add(root);
+        pos.add(new int[]{0,0}); // col, row
+        while(!q.isEmpty()){
+            TreeNode node=q.poll();
+            int[] p=pos.poll();
+            int col=p[0];
+            int row=p[1];
+            map.putIfAbsent(col,new TreeMap<>());
+            map.get(col).putIfAbsent(row,new PriorityQueue<>());
+            map.get(col).get(row).add(node.val);
+            if(node.left!=null){
+                q.add(node.left);
+                pos.add(new int[]{col-1,row+1});
+            }
+            if(node.right!=null){
+                q.add(node.right);
+                pos.add(new int[]{col+1,row+1});
+            }
+        }
+        for(TreeMap<Integer,PriorityQueue<Integer>> rows:map.values()){
+            List<Integer> list=new ArrayList<>();
+            for(PriorityQueue<Integer> pq:rows.values()){
+                while(!pq.isEmpty()){
+                    list.add(pq.poll());
+                }
+            }
+            res.add(list);
+        }
+        return res;
     }
 }
