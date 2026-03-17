826c8be4ef22874f422a9a211d38408e8bab2b6e
Anantika09 <anantikaag06@gmail.com>
2026-03-17 15:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (10.94%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,17 +15,29 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        trev(root,ans);
-        return ans;
-        
-    }
-    public void trev(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
+        List<Integer> res=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                res.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode pre=curr.left;
+                while(pre.right!=null && pre.right!=curr){
+                    pre=pre.right;
+                }
+                if(pre.right==null){
+                    pre.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    pre.right=null;
+                    res.add(curr.val);
+                    curr=curr.right;
+                }
+            }
         }
-        trev(root.left,ans);
-        ans.add(root.val);
-        trev(root.right,ans);
+        return res;
     }
 }
