2d284b9ae0730b6aff670950e1c19dae59fcc3ef
Anantika09 <anantikaag06@gmail.com>
2026-03-17 10:52:04 +0530

    Time: 4 ms (61.73%) | Memory: 44.5 MB (13.01%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,68 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        // Map: col -> Map: row -> PriorityQueue of values
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-
-        // BFS queue: store node with its col and row
-        Queue<Tuple> q = new LinkedList<>();
-        q.offer(new Tuple(root, 0, 0));
-
-        while (!q.isEmpty()) {
-            Tuple t = q.poll();
-            TreeNode node = t.node;
-            int col = t.col;
-            int row = t.row;
-
-            map.putIfAbsent(col, new TreeMap<>());
-            map.get(col).putIfAbsent(row, new PriorityQueue<>());
-            map.get(col).get(row).offer(node.val);
-
-            if (node.left != null) {
-                q.offer(new Tuple(node.left, col - 1, row + 1));
+        TreeMap<Integer,TreeMap<Integer,List<Integer>>> m=new TreeMap<>();
+        fun(root,m,0,0);
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int p:m.keySet()){
+            List<Integer> l=new ArrayList<>();
+            TreeMap<Integer,List<Integer>> level=m.get(p);
+            for(List<Integer> nodes:level.values()){
+                Collections.sort(nodes);
+                l.addAll(nodes);
             }
-            if (node.right != null) {
-                q.offer(new Tuple(node.right, col + 1, row + 1));
-            }
+            ans.add(l);
         }
-
-        List<List<Integer>> res = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
-            List<Integer> colList = new ArrayList<>();
-            for (PriorityQueue<Integer> pq : rows.values()) {
-                while (!pq.isEmpty()) {
-                    colList.add(pq.poll());
-                }
-            }
-            res.add(colList);
-        }
-
-        return res;
+        return ans;
     }
-
-    static class Tuple {
-        TreeNode node;
-        int col;
-        int row;
-        Tuple(TreeNode n, int c, int r) {
-            node = n;
-            col = c;
-            row = r;
-        }
+    void fun(TreeNode root,TreeMap<Integer,TreeMap<Integer,List<Integer>>> m,int r,int c){
+        if(root==null) return;
+        TreeMap<Integer,List<Integer>> p=m.computeIfAbsent(c,k->new TreeMap<>());
+        List<Integer> l=p.computeIfAbsent(r,k->new ArrayList<>());
+        l.add(root.val);
+        fun(root.left,m,r+1,c-1);
+        fun(root.right,m,r+1,c+1);
     }
 }
